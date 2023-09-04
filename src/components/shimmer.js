73ae3a98285fa0684ccdf5554228386853a4b15d
Kerthik  S <kerthikcyc@gmail.com>
2023-09-04 19:03:54 +0530

    shimmer ui

--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -1,9 +1,18 @@
-const Shimmer=()=>{
-    return(
-        <div className="Restranut-list">
-            {Array(19).fill("").map((e,index)=> <div key={index} className="shimmer-card"></div>)}
-           
-        </div>
+const Shimmer = () => {
+    return (
+      <div className="flex flex-wrap">
+        {Array(19)
+          .fill("")
+          .map((_, index) => (
+            <div key={index} className="w-1/4 p-4">
+              <div className="w-full h-40 bg-gray-300 animate-pulse"></div>
+              <div className="mt-2 w-4/5 h-6 bg-gray-300 animate-pulse"></div>
+              <div className="mt-2 w-2/3 h-6 bg-gray-300 animate-pulse"></div>
+            </div>
+          ))}
+      </div>
     );
-};
-export default Shimmer;
+  };
+  
+  export default Shimmer;
+  
